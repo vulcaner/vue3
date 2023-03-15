e8621afec7debc38fe5a704c0ec0a0c727605a38
lixiaocong <xiaocong.lee@icloud.com>
2023-03-15 20:35:08 +0800

    vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,49 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+import { visualizer } from "rollup-plugin-visualizer";
+
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  // const env = loadEnv(mode, process.cwd(), '')
+  console.log(mode)
+  // console.log(env)
+  const pluginsArr = mode === "production" ? [visualizer({
+    open: true,
+    template: "network",//treemap
+  })] : []
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+        dts: false
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+        // dts: false
+      }),
+      ...pluginsArr,
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-  // https://cn.vitejs.dev/config/shared-options.html#base
-  base: "/",
+    // https://cn.vitejs.dev/config/shared-options.html#base
+    base: "/",
+    build: {
+      sourcemap: true,
+      reportCompressedSize: true,
+    },
+  }
 });
