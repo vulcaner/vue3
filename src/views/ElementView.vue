51b4eb9b262ffa492c8b5f47a4a022f697fb5c52
lixiaocong <xiaocong.lee@icloud.com>
2023-03-15 18:51:18 +0800

    add element-plus

---
+++ src/views/ElementView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>ele</main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script setup lang="ts">
+import { onMounted } from "vue";
+onMounted(() => { });
+</script>
